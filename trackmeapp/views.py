a4c0e4d16fa8e464b56b326c4c64e51873c1c622
EnockOMONDI <enockomondike@gmail.com>
2019-06-22 06:47:05 +0300

    added api view for delete & put

--- trackmeapp/views.py
+++ trackmeapp/views.py
@@ -34,3 +34,30 @@ def get_post_items(request):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+'''
+DELETE and PUT requests on a specific t object
+'''
+
+
+@api_view(['DELETE', 'PUT'])
+def update_delete_items(request, pk):
+    try:
+        item = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        content = {
+            'status': 'Not Found'
+        }
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+     # update an item
+    if request.method == 'PUT':
+        serializer = TaskSerializer(item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # delete an item
+    elif request.method == 'DELETE':
+        item.delete()
+        return Response('Success', status=status.HTTP_202_ACCEPTED)
