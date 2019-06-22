cafe5acf9add3997aca9bbaac388a8f0499e00e6
EnockOMONDI <enockomondike@gmail.com>
2019-06-22 06:42:38 +0300

    added get & post view

--- trackmeapp/views.py
+++ trackmeapp/views.py
@@ -17,3 +17,20 @@ def index(request, template_name='index.html'):
 handle GET and POST requests, list of available Task or creating a new Task
 '''
 
+
+# get all items
+@api_view(['GET', 'POST'])
+def get_post_items(request):
+    if request.method == 'GET':
+        puppies = Task.objects.all()
+        serializer = TaskSerializer(puppies, many=True)
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    # create a new item
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
