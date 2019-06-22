907b3224f77bac7141dc5b741ae44917abf234ce
EnockOMONDI <enockomondike@gmail.com>
2019-06-22 06:37:28 +0300

    added index view

--- trackmeapp/views.py
+++ trackmeapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from trackmeapp.models import Task
+# from trackmeapp.serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+'''
+handle index
+'''
+def index(request, template_name='index.html'):
+    return render(request, template_name)
+
+
+'''
+handle GET and POST requests, list of available Task or creating a new Task
+'''
+
